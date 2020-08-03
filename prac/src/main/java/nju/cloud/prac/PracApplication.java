6fc788aed33bb4d2948c7766896ab1b8ef42d914
pkun <181250068@smail.nju.edu.cn>
2020-08-03 21:54:07 +0800

    feat: add premetheus

--- prac/src/main/java/nju/cloud/prac/PracApplication.java
+++ prac/src/main/java/nju/cloud/prac/PracApplication.java
@@ -1,13 +1,22 @@
 package nju.cloud.prac;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PracApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PracApplication.class, args);
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(@Value("S{spring.application.name}") String applicationName){
+        return (registry -> registry.config().commonTags("application", applicationName));
     }
 
 }
